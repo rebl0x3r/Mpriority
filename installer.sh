af4071b8ec674809c858eecc27d97b9e71df32f3
mrblackx <63623649+rebl0x3r@users.noreply.github.com>
2021-03-02 23:42:20 +0100

    installer.sh
    - Needs Root!
    
    sudo ./installer.sh

---
+++ installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+apt update -y
+apt upgrade -y
+apt install apache2 -y
+systemctl start apache2.service
+apt install -y php-{bcmath,bz2,intl,gd,mbstring,mcrypt,mysql,zip} libapache2-mod-phpy
+systemctl enable apache2.service
+systemctl restart apache2.service
+
+read -p "Enter hostname(ex:supporter.mail.com): " mail
+
+hostname $mail
+
+apt install postfix -y
+cd /var/www/html
+
+wget https://github.com/rebl0x3r/Mpriority/blob/main/Mpriority.zip?raw=true
+
+sudo apt install unzip -y
+
+unzip priority.zip
+
+echo -e "\033[32m[*] \033[37mInstallation is done!"
+
